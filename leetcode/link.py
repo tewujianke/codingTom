4a023d8bb43738a58d397b294e1bff8bb2eade3f
guanduoli <liguanduo@gmail.com>
2019-03-29 23:22:17 -0700

    Run this script will create a soft link pointing to the latest problem you worked on, for convinence

---
+++ leetcode/link.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""
+A script to create a soft link pointing to the most recent problem.
+Will glob all directories and find the most recent created one.
+"""
+import glob
+import os
+
+if os.path.islink("latest"):
+    print("Existed latest symlink.")
+    os.unlink("./latest")
+files = glob.glob("*")
+
+
+dirs = list(filter(lambda x:os.path.isdir(x),files))
+print("found dirs:")
+print (list(dirs))
+dirs.sort(key=lambda x: os.path.getmtime(x))
+print("sorted dir:")
+print(dirs)
+src = "./"+dirs[-1]
+
+print("New pointer to "+src)
+os.symlink(src,"./latest")
