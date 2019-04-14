4d189657480caaa7bc05d86ffd6034f921ee51e3
tom <spoubamire@gmail.com>
2019-04-14 00:31:57 -0700

    add comment

--- leetcode/maximumSubarray/tom/maximumSubarry.py
+++ leetcode/maximumSubarray/tom/maximumSubarry.py
@@ -20,14 +20,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        tmp = 0
+        tmp = 0                     #### tmp adds every element in nums
         maxVal = float('-inf')
         for n in nums :
             if n >= 0 and tmp < 0 :
-                tmp = n
+                tmp = n             ### if we find tmp < 0, it means tmp + n < n which is not largest sum. Therefore, we reset the tmp to n
             elif n < 0 and tmp < 0 :
-                tmp = max(n,tmp)
+                tmp = max(n,tmp)    ### if n and tmp are all negative, we need to store the larest one 
             else :
-                tmp = tmp + n
+                tmp = tmp + n       
             maxVal = max(maxVal,tmp)
         return (maxVal)
