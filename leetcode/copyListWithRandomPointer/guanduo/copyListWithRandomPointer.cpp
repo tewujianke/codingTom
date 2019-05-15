86776f36f1d1dc06646d6f8469a73c62bff9bb6b
guanduoli <liguanduo@gmail.com>
2019-05-14 21:23:42 -0700

    Copy List with Random Pointer c++

---
+++ leetcode/copyListWithRandomPointer/guanduo/copyListWithRandomPointer.cpp
@@ -0,0 +1,31 @@
+/*
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+*/
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node() {}
+
+    Node(int _val, Node* _next, Node* _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        
+    }
+};
