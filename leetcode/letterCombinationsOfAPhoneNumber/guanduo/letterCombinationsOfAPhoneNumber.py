b2cff6c1ba9e20fc5b004212c07b693de21385b8
guanduoli <liguanduo@gmail.com>
2019-05-23 17:54:56 -0700

    letter combinations of a phone number

---
+++ leetcode/letterCombinationsOfAPhoneNumber/guanduo/letterCombinationsOfAPhoneNumber.py
@@ -0,0 +1,20 @@
+"""
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+
+Example:
+
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+"""
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        
