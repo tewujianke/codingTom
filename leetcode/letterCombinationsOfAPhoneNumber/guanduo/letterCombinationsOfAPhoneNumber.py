42bb96232f675d8fac9e929f05556d150e5fb757
guanduoli <liguanduo@gmail.com>
2019-05-23 18:12:42 -0700

    finished using itertool product

--- leetcode/letterCombinationsOfAPhoneNumber/guanduo/letterCombinationsOfAPhoneNumber.py
+++ leetcode/letterCombinationsOfAPhoneNumber/guanduo/letterCombinationsOfAPhoneNumber.py
@@ -11,10 +11,18 @@ Input: "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 """
 
-class Solution(object):
+"""
+Use itertools to get the cartesian product
+
+"""
+from itertools import product
+class Solution:
     def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        
+        if len(digits) == 0: return []           #empty list
+        lookup = {'1':'','2':'abc','3':'def',    #my lookup table (dict)
+                  '4':'ghi','5':'jkl','6':'mno',
+                  '7':'pqrs','8':'tuv','9':'wxyz'}
+        list_char = [lookup[x] for x in digits]      #from the input digits, lookup all chars mapped
+        result_in_tuple = product(*list_char)                 #return a list of tuple with catesian product
+        result = [''.join(x) for x in result_in_tuple]        #combine the tuple into strings
+        return result
