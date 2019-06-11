ef7d83111bd8dfd2142a9ada960d3bff18b246eb
guanduoli <liguanduo@gmail.com>
2019-06-11 00:58:47 -0700

    finish both BFS and DFS

--- leetcode/checkCompletenessOfABinaryTree/guanduo/checkCompletenessOfABinaryTree.py
+++ leetcode/checkCompletenessOfABinaryTree/guanduo/checkCompletenessOfABinaryTree.py
@@ -33,13 +33,21 @@ The tree will have between 1 and 100 nodes.
 #         self.right = None
 """
 """
+Using TWO methods to solve.
+1. 
 Use BFS. Push all nodes to a queue, including all NULL ptr children
 Once we see a None, then the following object in the queue must not be a valid node. 
 Otherwise, it is not a complete tree.
+
+2.
+Use DFS. Get max valid depth by peaking into left-end.
+When we have reached the first None at depth, raise a flag. With a raised flag, if a valid node appears at same
+level (depth), return False.
+Corner cases to deal with: all previous levels must be full. If there is a deeper right branch(level > depth), return false
 """
 from collections import deque
 class Solution(object):
-    def isCompleteTree(self, root):
+    def isCompleteTreeBFS(self, root):
         """
         :type root: TreeNode
         :rtype: bool
@@ -59,3 +67,33 @@ class Solution(object):
             que.append(cur.right)
             
         return True
+
+    """
+    Use DFS to solve.
+    """
+    def isCompleteTreeDFS(self, root):
+        if not root: return True
+        depth = 0
+
+        node = root            #calculate the max depth on the left
+        while node:
+            depth+=1
+            node = node.left
+            
+        def DFS(node,level=0,flag=False):
+            """
+            return (flag,isComplete)
+            """
+            if not flag and not node and level==depth: return(True,True)  #first None at depth. Raise flag
+            if not node: return (flag,True)                               #regular NULL ptr, jsut return a complete result
+            if level <=depth-2:                                           #all previous levels must be full
+                if not node.left or not node.right: return (flag,False)   #means both children present at depth-2 and above
+            if level > depth: return(flag,False)                          #a deeper right branch
+            if flag and level==depth and node: return(flag,False)         #invalid node as flag has been raised
+            if not node.left and node.right: return(flag,False)           #go left first
+            flag,isComplete = DFS(node.left,level+1,flag)
+            if not isComplete: return (flag,False)
+            flag,isComplete = DFS(node.right,level+1,flag)                #then go right
+            return (flag,isComplete)
+        flag,isComplete = DFS(root,1)
+        return isComplete
